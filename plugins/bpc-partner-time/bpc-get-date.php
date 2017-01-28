759e76259c297ecd4c76ce23795ba0d6be9dacde
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2017-01-29 00:14:37 +0800

    updated and working get date

--- plugins/bpc-partner-time/bpc-get-date.php
+++ plugins/bpc-partner-time/bpc-get-date.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/wp-load.php");
-$datenow = $_GET['datenow']; 
+$datenow = $_GET['datenow'];
 
-	$partner_id = 77332; 
+	$partner_id = 77332;
 	$datepiece = explode("/", $datenow); // 22/01/2017 -dd/mm/yyyy
 	$datetofetch = $datepiece[2].'-'.$datepiece[1].'-'.$datepiece[0]; //2017-01-16 - yyyy-mm-dd
 
 	$mydb = new wpdb('dbo639369002','1qazxsw2!QAZXSW@','db639369002','db639369002.db.1and1.com');
-	$rows = $mydb->get_results("SELECT * FROM wp_bpc_appointment_settings WHERE partner_id = $partner_id 
+	$rows = $mydb->get_results("SELECT * FROM wp_bpc_appointment_settings WHERE partner_id = $partner_id
 			&& date = '$datetofetch'");
 //	echo "<ul>";
 		foreach ($rows as $obj) :
@@ -35,27 +35,24 @@ $datenow = $_GET['datenow'];
 		//	echo "</ul>";
 	?>
 
+
         <?php
-        
+
 		$begin = new DateTime($open_from);
 		$end   = new DateTime($open_to);
 		$interval = DateInterval::createFromDateString($call_back_length);
 		$times    = new DatePeriod($begin, $interval, $end);
-
-		if(false):
 		?>
+		<ul class="left-time">
 		<?php
-
 		$currenttime = date('H:i');
 		$currentdate = date("Y-m-d");
 		$callbackdelayandcurrenttime = strtotime($call_back_delay, strtotime($updatedtime));
 		$callbackdelayandupdatedtimetotal = date('H:i', $callbackdelayandcurrenttime);
-		// echo 'Updated Time: '.$updatedtime.'<br>';	
-		// echo 'Total of Call Back Delay Time: '.$callbackdelayandupdatedtimetotal.'<br>';	
-		// echo 'Current Time: '.$currenttime.'<br>';	
+		// echo 'Updated Time: '.$updatedtime.'<br>';
+		// echo 'Total of Call Back Delay Time: '.$callbackdelayandupdatedtimetotal.'<br>';
+		// echo 'Current Time: '.$currenttime.'<br>';
 		$container = 0;
-		$counter1=0;
-		$counter2=0;
 
 		foreach ($times as $time) {
 			if(($time->format('H:i') >= $currenttime) and ($time->format('H:i') <= '12:59') and ($date == $currentdate))
@@ -64,59 +61,34 @@ $datenow = $_GET['datenow'];
 				{
 					//echo 'In between the Call Back Delay';
 				} else {
-
-					$counter1++;
-
-					$container = 1; 		
-				?>
-				<li><input name="time" value="<?php echo $time->format('H:i A');?>" type="radio" id="e3ve-time" class="e3ve-cl-time" onclick=timeFunction()><span class="e3ve-cl-times"><?php echo $time->format('H:i A'); ?></span></li>
-				<?php
-
-				}
-
-			} else if(($time->format('H:i') <= '12:59') and ($date <> $currentdate)) {
-				$counter1++;
-				?>
-				<li><input name="time" value="<?php echo $time->format('H:i A');?>" type="radio" id="left-time" class="e3ve-cl-time" onclick=timeFunction()><span class="e3ve-cl-times"><?php echo $time->format('H:i A'); ?></span></li>
-				<?php
-			}
-		    // echo $time->format('H:i'), '-', 
-		    // $time->add($interval)->format('H:i'), "<br>";
-		}
-		endif;
-		?>
-
-		<?php
-		$timeField = [];
-		$counter=0;
-		foreach ($times as $time) {
-			if(($time->format('H:i') >= $currenttime) and ($time->format('H:i') > '12:59') and ($date == $currentdate)){
-				if( ($updateddate == $currentdate) and ($time->format('H:i') >= $updatedtime) and ($time->format('H:i') <= $callbackdelayandupdatedtimetotal) )
-				{
-					//echo 'In between the Call Back Delay';
-				} else {
 					$container = 1;
-
-					$timeA = $time->format('h:i A');
 					$timeA = $time->format('h:i A');
 					$timeField[] = "<li><input name='time' value='$timeA' type='radio' id='e3ve-time' class='e3ve-cl-time' onclick='timeFunction()' ><span class='e3ve-cl-times'>$timeA</span></li>";
 
 				}
-			} else   {
+			} else if(($time->format('H:i') <= '12:59') and ($date <> $currentdate)) {
 				$timeA = $time->format('h:i A');
-
 				$timeField[] = "<li><input name='time' value='$timeA' type='radio' id='e3ve-time' class='e3ve-cl-time' onclick='timeFunction()' ><span class='e3ve-cl-times'>$timeA</span></li>";
 
 			}
+		    // echo $time->format('H:i'), '-',
+		    //      $time->add($interval)->format('H:i'), "<br>";
 		}
 
-		$timeResultsArr = bpc_getTimeResults($timeField);
-		bpc_print_time($timeResultsArr);
+
+
+		if(!empty($timeField)) {
+			$timeResultsArr = bpc_getTimeResults($timeField);
+			bpc_print_time($timeResultsArr);
+		} else {
+			bpc_print_no_time_display();
+		}
+
 
 		?>
         <script>
         	function timeFunction()
-			{ 
+			{
 				if ( $("#callbackdate").val().length > 0 ) {
 		 			document.getElementById('datepicked').innerHTML = $datenow;
 		 			document.getElementById('timeselected').innerHTML = ($("input[name=time]:checked").val());
@@ -124,10 +96,34 @@ $datenow = $_GET['datenow'];
 					jQuery('.home-step-1').delay(500).slideUp(500);
 					jQuery('.home-step-3').delay(500).slideUp(500);
 					jQuery('.home-step-4').delay(500).slideUp(500);
- 				}	
+ 				}
 			}
         </script>
         <?php
+
+
+
+        function bpc_print_no_time_display()
+        {
+        ?>
+        	<div class="home-time-box">
+			  <div class="home-time-box-heading">
+				<div class="home-time-box-heading-left">
+
+				</div>
+				<div class="home-time-box-heading-right">
+				  <h3>
+				  </h3>
+				</div>
+			  </div>
+			  <div class="home-time-content">
+				<div class="e3ve-home-time" style="height: 197px;">
+					<h1>No Schedule Display</h1>
+				</div>
+			  </div>
+			</div>
+        <?php
+        }
 		function bpc_print_time($timeResultsArr)
 {
 	$arrowNext = -1;
